1607d08c988f3cd9b52362fd19c810c6570416de
ilkin3333 <ihumbetov09@gmail.com>
2024-08-28 17:59:42 +0400

    Mobile responsive

--- components/ui/MobileNav.tsx
+++ components/ui/MobileNav.tsx
@@ -40,7 +40,7 @@ const MobileNav = () => {
                <div className='mt-32 mb-40 text-center text-2xl'>
                 <Link href="/">
                 <h1 className='text-4xl font-semibold'>
-                    Humbatov <span className='text-accent'></span></h1>
+                    Saythub <span className='text-accent'></span></h1>
                 </Link>
                </div>
                <nav className='flex flex-col justify-center items-center gap-8'>
